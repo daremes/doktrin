61a94bbdd2c909cec0c14fe90da0617a4ab0a8a8
Martin Semerad <martin.sem84@gmail.com>
2021-11-18 00:43:30 +0100

    test api

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -1,3 +1,21 @@
-export default function handler(req: any, res: any) {
-  res.status(200).json({ name: "John Doe" });
+import { getAllDocs, getJsTimestamp } from "../../firebase/firebase";
+import { getSlug } from "../../utils/slug";
+
+export default async function handler(req: any, res: any) {
+  const articles: any[] = [];
+  const collectionData = await getAllDocs("articles");
+
+  collectionData.forEach((doc) => {
+    const data = doc.data();
+    const timestamp = getJsTimestamp(data.created);
+
+    articles.push({
+      ...data,
+      slug: getSlug(data.title, timestamp),
+      docId: doc.id,
+      created: timestamp,
+    });
+  });
+  console.log("server", articles);
+  res.status(200).json(articles);
 }
